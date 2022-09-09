eb798c6b9973dcf42e18b87d9e8d4b3c19e2de03
navidmirzad <navi0221@stud.kea.dk>
2022-09-09 13:36:47 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,10 @@
+public class SuperGreeter {
+    public void start() {
+        System.out.println("Det virker!");
+    }
+
+    public static void main (String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+    }
+}
