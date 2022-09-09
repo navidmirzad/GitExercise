b8d545cb97d1c78eab495eb3cd96c9f2899ef432
navidmirzad <navi0221@stud.kea.dk>
2022-09-09 13:55:07 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
 public class SuperGreeter {
+    Scanner sc = new Scanner(System.in);
+
     public void start() {
         System.out.println("Det virker!");
+        greet();
+        askAboutAge();
+    }
+
+    public void greet() {
+        System.out.println("Hej, hvad hedder du? ");
+        String indtastnavn = sc.next();
+        System.out.println("Hej!" + " " + indtastnavn);
+    }
+
+    public void askAboutAge() {
+        System.out.println("Hvor gammel er du? ");
+        int alder = sc.nextInt();
+        System.out.println(alder);
     }
 
     public static void main (String[] args) {
         SuperGreeter sg = new SuperGreeter();
-        SuperGreeter greet = new SuperGreeter();
         sg.start();
-
-        // lade bruger indtaste navn
-        System.out.println("Indtast dit navn: ");
-        Scanner sc = new Scanner(System.in);
-        String indtastnavn = sc.next();
-        System.out.println("Hej!" + " " + indtastnavn);
-
 
     }
 }
